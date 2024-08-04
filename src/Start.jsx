416fc7013c694b7590fe4a537e6d8b3eaac2abee
jeyachandran17 <jeyachandran1733@gmail.com>
2024-08-04 15:15:52 +0530

    change btn in start page

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,7 +1,13 @@
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Fingerprint from '@mui/icons-material/Fingerprint';
+
 export function Start() {
   return (
     <div className="login">
-      <button className='start-btn'>Start</button>
+      <Button variant="outlined" className='start-btn' color='secondary'>start<IconButton aria-label="fingerprint" color="secondary">
+        <Fingerprint />
+      </IconButton></Button>
     </div>
   );
 }
