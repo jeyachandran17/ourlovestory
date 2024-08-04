1c823e624e8b991c16a98eb21025ff49d621b608
jeyachandran17 <jeyachandran1733@gmail.com>
2024-08-04 14:51:56 +0530

    start page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Start } from './Start'
 
 function App() {
 
   return (
     <div className="App">
-
+      <Start />
     </div>
   )
 }
---
+++ src/Start.jsx
@@ -0,0 +1,7 @@
+export function Start() {
+  return (
+    <div className="login">
+      <button className='start-btn'>Start</button>
+    </div>
+  );
+}
